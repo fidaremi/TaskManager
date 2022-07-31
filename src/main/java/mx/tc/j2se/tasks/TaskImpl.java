e6271d5d1c8a6deb1f9ae9522b21d760c97fdd4f
fidaremi <fidatemi@gmail.com>
2022-07-30 20:08:19 -0500

    Practice 1 Second Commit

--- src/main/java/mx/tc/j2se/tasks/TaskImpl.java
+++ src/main/java/mx/tc/j2se/tasks/TaskImpl.java
@@ -9,7 +9,6 @@ package mx.tc.j2se.tasks;
  * @author Mariia Kuntsevych
  * Copyright notice: freeware
  */
-
 public class TaskImpl implements Task {
 
     private String title;
@@ -21,16 +20,18 @@ public class TaskImpl implements Task {
     private int interval;
     private int current;
 
-    /**
-     * This empty constructor doesn't set any properties
-     */
 
+    /**
+     * This empty constructor constructs a task and doesn't set any properties
+     */
     public TaskImpl() {
-    };
+    }
 
     /**
      * This constructor constructs an inactive task to run at a specified time
      * without repeating with a given name
+     * @param title is the argument which sets a tasks title
+     * @param time is the argument which sets a start time of non-repetitive task
      */
     public TaskImpl(String title, int time) {
         this.active = false;
@@ -43,6 +44,10 @@ public class TaskImpl implements Task {
      * This constructor constructs an inactive task to run within the specified time range
      * (including the start and the end time)
      * with the set repetition interval and with a given name
+     * @param title is the argument which sets a tasks title
+     * @param start is the argument which sets start time of repetitive task
+     * @param end is the argument which sets end time of repetitive task
+     * @param interval is the argument which sets interval for task repetition
      */
     public TaskImpl(String title, int start, int end, int interval) {
         this.active = false;
@@ -55,6 +60,7 @@ public class TaskImpl implements Task {
 
     /**
      * This method implements reading of the task name
+     * @return
      */
     public String getTitle() {
         return title;
@@ -62,6 +68,7 @@ public class TaskImpl implements Task {
 
     /**
      * This method implements setting of the task name
+     * @param title is the argument which sets a tasks title
      */
     public void setTitle(String title) {
         this.title = title;
@@ -69,6 +76,7 @@ public class TaskImpl implements Task {
 
     /**
      * This method implements reading of the task status
+     * @return
      */
     public boolean isActive() {
         return active;
@@ -76,6 +84,7 @@ public class TaskImpl implements Task {
 
     /**
      * This method implements setting of the task status
+     * @param active is the argument which sets task to active status
      */
     public void setActive(boolean active) {
         this.active = active;
@@ -85,6 +94,7 @@ public class TaskImpl implements Task {
      * This method implements returning repetition start time of the task
      * if the task is a repetitive one and execution time is the task is
      * non-repetitive
+     * @return
      */
     public int getTime() {
         if (repeated == true) {
@@ -96,6 +106,7 @@ public class TaskImpl implements Task {
     /**
      * This method implements setting of start time of non-repetitive
      * task and makes repetitive task non-repetitive
+     * @param time is the argument which sets a start time of non-repetitive task
      */
     public void setTime(int time) {
         if (repeated == true) {
@@ -106,6 +117,7 @@ public class TaskImpl implements Task {
 
     /**
      * This method returns the start time of the execution
+     * @return
      */
     public int getStartTime() {
         if (repeated == false) {
@@ -116,6 +128,7 @@ public class TaskImpl implements Task {
 
     /**
      * This method returns the end time of the execution
+     * @return
      */
     public int getEndTime() {
         if (repeated == false) {
@@ -127,6 +140,7 @@ public class TaskImpl implements Task {
     /**
      * This method returns repeat interval for repetitive task and
      * returns 0 for non-repetitive task
+     * @return
      */
     public int getRepeatInterval() {
         if (repeated == false) {
@@ -138,6 +152,9 @@ public class TaskImpl implements Task {
     /**
      * This method is setting start and end time of the repetitive tasks and
      * repeat interval for them, and makes non-repetitive task repetitive
+     * @param start is the argument which sets start time of repetitive task
+     * @param end is the argument which sets end time of repetitive task
+     * @param interval is the argument which sets interval for task repetition
      */
     public void setTime(int start, int end, int interval) {
         this.start = start;
@@ -148,6 +165,7 @@ public class TaskImpl implements Task {
 
     /**
      * This method is checking the task for repeatability
+     * @return
      */
     public boolean isRepeated() {
         return repeated;
@@ -156,6 +174,8 @@ public class TaskImpl implements Task {
     /**
      * This method is checking the task for repeatability
      * and returns its next fire time.
+     * @param current is the argument which set the current moment of time
+     * @return
      */
     public int nextTimeAfter (int current) {
         if (active && repeated == false) {
@@ -179,5 +199,23 @@ public class TaskImpl implements Task {
             return -1;
         }
         return -1;
+    }
+
+    /**
+     * This method returns task's parameters are a string values
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "TaskImpl{" +
+                "title='" + title + '\'' +
+                ", time=" + time +
+                ", start=" + start +
+                ", end=" + end +
+                ", active=" + active +
+                ", repeated=" + repeated +
+                ", interval=" + interval +
+                ", current=" + current +
+                '}';
     }
 }
